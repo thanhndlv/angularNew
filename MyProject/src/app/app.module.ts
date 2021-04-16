2298203269d6541495998937c60364a9b58c69fc
thanhndlv <thanhndlv@gmail.com>
2021-04-16 16:08:10 +0800

    new update

--- MyProject/src/app/app.module.ts
+++ MyProject/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ServeComponent } from './serve/serve.component';
+import { ServerComponent } from './server/server.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServeComponent,
+    ServerComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
